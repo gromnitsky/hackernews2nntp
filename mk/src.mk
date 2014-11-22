87e0e14d0629ebf651643c452b10d413806e1cac
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2014-11-22 08:12:21 +0200

    fix scr/ to lib/ compilation in absent of lib/

--- mk/src.mk
+++ mk/src.mk
@@ -6,11 +6,12 @@ json := $(subst src/, lib/, $(wildcard src/*.json))
 clean += $(json) $(js)
 
 lib/%.json: src/%.json
+	@mkdir -p $(dir $@)
 	cp $< $@
 
 lib/%.js: src/%.coffee
+	@mkdir -p $(dir $@)
 	$(cs_compiler) -c -o $(dir $@) $<
 
 .PHONY: compile
 compile: $(json) $(js)
-	@mkdir -p lib
