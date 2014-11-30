2e66fd6504e3f7b1257552fb85c24f4278805fc3
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2014-11-30 21:20:10 +0200

    copy template/job.txt from template/story.txt
    because <censored> npm ignores symlinks

--- mk/src.mk
+++ mk/src.mk
@@ -3,7 +3,7 @@ cs_compiler := node_modules/.bin/coffee
 js := $(subst src/, lib/, $(patsubst %.coffee, %.js, $(wildcard src/*.coffee)))
 json := $(subst src/, lib/, $(wildcard src/*.json))
 
-clean += $(json) $(js)
+clean += $(json) $(js) template/job.txt
 
 lib/%.json: src/%.json
 	@mkdir -p $(dir $@)
@@ -13,5 +13,9 @@ lib/%.js: src/%.coffee
 	@mkdir -p $(dir $@)
 	$(cs_compiler) -c -o $(dir $@) $<
 
+# npm doesn't pack symlinks :(
+template/job.txt: template/story.txt
+	cp $< $@
+
 .PHONY: compile
-compile: $(json) $(js)
+compile: $(json) $(js) template/job.txt
